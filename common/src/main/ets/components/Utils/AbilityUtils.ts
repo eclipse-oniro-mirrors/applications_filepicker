d4fb5c4af090417ecfe9564b87cb6ed002cc5e42
w00356339 <wangdefei1@huawei.com>
2023-02-07 13:03:19 +0800

    选择文件和保存文件后uri对应key新增
    Signed-off-by: wonder <769425456@qq.com>

--- common/src/main/ets/components/Utils/AbilityUtils.ts
+++ common/src/main/ets/components/Utils/AbilityUtils.ts
@@ -25,6 +25,7 @@ export function terminateSelfWithResult(resultCode: number, result: string): voi
 		want: {
 			parameters: {
 				'startMode': startMode,
+				'result': result,
 				'select_item_list': [result],
 				'pick_path_return': [result]
 			}
